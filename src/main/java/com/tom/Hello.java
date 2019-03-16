deec9a3c6fa8b1089d284bc98c24c7c3d7149f73
kming-kotlin <ming@tajen.edu.tw>
2019-03-16 23:40:56 +0800

    Initial commit

---
+++ src/main/java/com/tom/Hello.java
@@ -0,0 +1,13 @@
+package com.tom;
+
+public class Hello {
+    public static void main(String[] args) {
+//        System.out.println("Hello World");
+//        new Person().hello();
+        Person p = new Person();
+        p.hello();
+
+
+    }
+
+}
